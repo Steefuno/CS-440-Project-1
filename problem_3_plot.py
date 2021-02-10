3d44443b3f24275a816c3c93b59c9730f91bda88
Steve <pickleman20@hotmail.com>
2021-02-09 20:09:28 -0500

    added a json file for data of problem_3 and renamed the png

--- problem_3_plot.py
+++ problem_3_plot.py
@@ -2,11 +2,12 @@ import matplotlib.pyplot as plt
 import maze
 import a
 import bfs
+import json
 
 width = 100
 height = 100
 tries = 100
-interval = .0625
+interval = .03125
 
 densities = [] # X axis
 avg_a_count = [] # Y axis 1
@@ -27,18 +28,30 @@ while density < 1: # for each density
         a_result = a.a([], m, (1, 1), (height - 2, width - 2))
         bfs_result = bfs.bfs([], m, (1, 1), (height - 2, width - 2))
 
-        if a_result != False:
-            a_count += a_result
-        if bfs_result != False:
-            bfs_count += a_result
+        a_count += a_result
+        bfs_count += bfs_result
     
     densities.append(density)
     avg_a_count.append(a_count / tries)
     avg_bfs_count.append(bfs_count / tries)
+    print(("Avg a count at density {density} is {count}").format(density = density, count = a_count / tries))
+    print(("Avg bfs count at density {density} is {count}").format(density = density, count = bfs_count / tries))
+
+data = json.dumps(
+    {
+        "densities" : densities,
+        "avg_a_count" : avg_a_count,
+        "avg_bfs_count" : avg_bfs_count,
+    }
+)
+file = open("./test_results/Number of Nodes Explored vs Density.json", "w+")
+file.write(data)
+file.close()
 
 plt.plot(densities, avg_a_count, label = "A*")
 plt.plot(densities, avg_bfs_count, label = "BFS")
 plt.xlabel("Densities")
 plt.ylabel("Average Number of Nodes")
-plt.title(("Number of Nodes Explored vs Density in Random {width} by {height} Maze").format(width = width, height = height))
+plt.legend()
+plt.title(("Number of Nodes Explored vs Density").format(width = width, height = height))
 plt.show()
